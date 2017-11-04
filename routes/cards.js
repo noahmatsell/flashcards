2493a772d367bfd8df2fe06a278ec9aff306a035
noahub <noah.matsell@unbounce.com>
2017-11-03 17:29:33 -0700

    cards update

--- routes/cards.js
+++ routes/cards.js
@@ -4,7 +4,13 @@ const {data} = require('../data/flashcardData.json');
 const {cards} = data;
 
 router.get('/:id', (req, res) => {
-    const {side} = req.query.side;
+    const {side} = req.query;
+    const {id} = req.params;
+    const text = cards[id][side];
+    const {hint} =  cards[id];
+
+    const templateData = P
+
     res.render('card', {
         prompt: cards[req.params.id].question, 
         hint: cards[req.params.id].hint
